13507c1b2aaac3d082878a62a5217914369b4ed2
vanndsu <99514321+vanndsu@users.noreply.github.com>
2025-06-13 21:12:52 -0300

    Add files via upload

---
+++ Introduction/dataTypes/script.js
@@ -0,0 +1,17 @@
+let userName = "Vanderson";
+let age = 90;
+let revenue = ["ovo", "farinha", "acucar"];
+let online = true;
+let fullName = {
+  firstName: "Francisco",
+  lastName: "Silva",
+  age: 45,
+};
+
+console.log(
+  typeof userName, //string
+  typeof age, //number
+  typeof revenue, //object
+  typeof online, //boolean
+  typeof fullName //object
+);
