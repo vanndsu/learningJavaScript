5371fa29b38652467c68d14a9fbae621ee4d3b79
vanndsu <99514321+vanndsu@users.noreply.github.com>
2025-06-18 22:04:47 -0300

    Add files via upload

---
+++ Introduction/semicolon/script.js
@@ -0,0 +1,5 @@
+let firstName = "Vanderson"; let lastname = "Ribeiro"; //O ponto e vírgula determina o final do código
+//sendo assim, após colocado, aquele código "acabou" e poderá começar outro.
+//por isso, quando colocado, é possível criar outro código na mesma linha.
+
+console.log("Meu nome completo " + firstName, lastname);
