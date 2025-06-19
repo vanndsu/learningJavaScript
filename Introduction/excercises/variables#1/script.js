e27086156f8d12c858c850ec16a3b639ee4dd9a1
vanndsu <99514321+vanndsu@users.noreply.github.com>
2025-06-18 21:43:54 -0300

    Add files via upload

---
+++ Introduction/excercises/variables#1/script.js
@@ -0,0 +1,10 @@
+// 1) Crie uma variável para armazenar o nome do carro (Ferrari) e a exiba-a.
+// 2) Crie uma variável para armazenar o preço do bolo que custa R$ 19,99. E a exiba-a.
+// 3) Corrija o código a seguir (e exiba-o). let "cidade" = São Paulo.
+
+let car = "Ferrari";
+console.log("Meu carro é uma:", car);
+let cakePrice = 19.99;
+console.log("O preço do bolo de chocolate é: R$", cakePrice);
+let cidade = "São Paulo";
+console.log("Cidade corrigida:", cidade);
